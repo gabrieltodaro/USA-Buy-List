91246bad94ebb6c6e60b656121e079cc3fce39aa
Gabriel Patane Todaro <gabriel.patane-todaro@podium.com>
2023-03-29 22:54:24 -0300

    Start with totals.

--- ComprasUSA/TotalViewController.swift
+++ ComprasUSA/TotalViewController.swift
@@ -5,12 +5,15 @@
 //  Created by Gabriel Patane Todaro on 07/03/23.
 //
 
+import CoreData
 import UIKit
 
 class TotalViewController: UIViewController {
 
   @IBOutlet weak var usdTotalLabel: UILabel!
   @IBOutlet weak var brlTotalLabel: UILabel!
+
+  private var addedProducts = [Product]()
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -19,8 +22,48 @@ class TotalViewController: UIViewController {
 
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
+    fetchProducts()
+    calculate()
+  }
+}
 
-    usdTotalLabel.text = "No Items yet"
-    brlTotalLabel.text = "No Items yet"
+// MARK: - Private Methods
+extension TotalViewController {
+  private func fetchProducts() {
+    let productsFetch: NSFetchRequest<Product> = Product.fetchRequest()
+
+    do {
+      let managedContext = AppDelegate.shared.coreDataStack.managedContext
+      let results = try managedContext.fetch(productsFetch)
+      addedProducts = results
+    } catch let error as NSError {
+      print("Fetch error: \(error) description: \(error.userInfo)")
+    }
+  }
+
+  private func calculate() {
+    guard let dolarConversionString = UserDefaults.standard.string(forKey: UserDefaultsKeys.dolar.rawValue),
+          let dolarConversation = Double(dolarConversionString),
+          let iofString = UserDefaults.standard.string(forKey: UserDefaultsKeys.iof.rawValue),
+          let iof = Double(iofString) else {
+      return
+    }
+
+    var totalInBRL: Double = 0
+    var totalInUSD: Double = 0
+
+    for product in addedProducts {
+      totalInUSD += product.value
+
+      if product.usingCard {
+        let valueWithIOF = product.value + ((product.value * iof) / 100)
+        totalInBRL += valueWithIOF
+      } else {
+        totalInBRL += product.value
+      }
+    }
+
+    usdTotalLabel.text = "US$ \(totalInUSD)"
+    brlTotalLabel.text = "R$ \(totalInBRL * dolarConversation)"
   }
 }
